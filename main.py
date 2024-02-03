edce9bd5e8a7c5bc03c2fd2ec4885b75a3463b97
Sebas <99556112+AstroGaze@users.noreply.github.com>
2024-02-02 21:24:07 -0600

    Practica de Diffie-Hellman

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ print("Ejercicio 1- obtener un numero de 256 bits usando la funcion random", ran
 i = 0
 while(True):
     i = i + 1
-    j = random.getrandbits(1024)
+    j = random.getrandbits(1536)
     esPrimo = number.isPrime(j)
     if(esPrimo):
         print("En la iteracion: ", i, " se econtro el primo: ", j, "\n")
